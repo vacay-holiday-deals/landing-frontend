678bd408f156b5c3710500840f902e1731468e50
newtfrank <mymbugua@gmail.com>
2019-06-13 12:50:30 +0300

    corrected share component

--- src/components/Share.js
+++ src/components/Share.js
@@ -2,15 +2,11 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShareAlt } from '@fortawesome/free-solid-svg-icons'
 import Navbar from 'react-bootstrap/Navbar'
-import Popover from 'react-bootstrap/Popover'
 
 function Share() {
   return (
     <div className='share'>
       <Navbar className='nav--appbar'>
-        <div className='hidden--popover'>
-          <Popover>Hello this is a popover</Popover>
-        </div>
         <a href='hello' className='instagram'>
           <span className='fontawesomeicon'>
             <FontAwesomeIcon icon={faShareAlt} className='icon' />
