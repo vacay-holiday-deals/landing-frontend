002dbbf147a01aa90da3303c6730e4091ff6bed9
newtfrank <mymbugua@gmail.com>
2019-06-13 10:25:00 +0300

    changed the call component

--- src/components/Call.js
+++ src/components/Call.js
@@ -1,54 +1,51 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faWhatsapp,
   faFacebook,
   faInstagram,
   faTwitter
-} from "@fortawesome/free-brands-svg-icons";
+} from '@fortawesome/free-brands-svg-icons'
 
-import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
 
 function Call() {
   return (
-    <div className="top-bar">
-      <div className="top-bar--contact">
-        <a href="tel:25416875656" className="top-bar--contact-info">
-          <FontAwesomeIcon icon={faPhone} className="icon" />
-          <span className="contact-links">Call us at : +254716 875656</span>
+    <div className='top-bar'>
+      <div className='top-bar--contact'>
+        <a href='tel:+254716875656' className='top-bar--contact-info'>
+          <FontAwesomeIcon icon={faPhone} className='icon' />
+          <span className='contact-links'>Call us at : +254716 875656</span>
         </a>
-        <a href="MAILTO:info@vacay.co.ke" className="top-bar--contact-info">
-          <FontAwesomeIcon icon={faEnvelope} className="icon" />
-          <span className="contact-links">Email us at : info@vacay.co.ke </span>
+        <a href='MAILTO:info@vacay.co.ke' className='top-bar--contact-info'>
+          <FontAwesomeIcon icon={faEnvelope} className='icon' />
+          <span className='contact-links'>Email us at : info@vacay.co.ke </span>
         </a>
       </div>
-      <div className="top-bar--social">
+      <div className='top-bar--social'>
         <a
-          href="https://www.instagram.com/vacayholidaydeals/?hl=en"
-          className="top-bar--social-links"
-        >
-          <FontAwesomeIcon icon={faInstagram} className="icon" />
+          href='https://www.instagram.com/vacayholidaydeals/?hl=en'
+          className='top-bar--social-links'>
+          <FontAwesomeIcon icon={faInstagram} className='icon' />
         </a>
 
         <a
-          href="https://www.facebook.com/vacaydeals/"
-          className="top-bar--social-links"
-        >
-          <FontAwesomeIcon icon={faFacebook} className="icon" />
+          href='https://www.facebook.com/vacaydeals/'
+          className='top-bar--social-links'>
+          <FontAwesomeIcon icon={faFacebook} className='icon' />
         </a>
 
         <a
-          href="https://twitter.com/Vacay_Deals"
-          className="top-bar--social-links"
-        >
-          <FontAwesomeIcon icon={faTwitter} className="icon" />
+          href='https://twitter.com/Vacay_Deals'
+          className='top-bar--social-links'>
+          <FontAwesomeIcon icon={faTwitter} className='icon' />
         </a>
-        <a href="https://wa.me/254716875656" className="top-bar--social-links">
-          <FontAwesomeIcon icon={faWhatsapp} className="icon" />
+        <a href='https://wa.me/254716875656' className='top-bar--social-links'>
+          <FontAwesomeIcon icon={faWhatsapp} className='icon' />
         </a>
       </div>
     </div>
-  );
+  )
 }
 
-export default Call;
+export default Call
