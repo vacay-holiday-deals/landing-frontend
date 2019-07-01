b85d0f8fc56f2e68901dde3f14a8870f7c46ddda
newtfrank <mymbugua@gmail.com>
2019-07-01 13:40:01 +0300

    changes on serverjs

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ const http = require('http')
 const helmet = require('helmet')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
 
+// instantiate and express app
 const app = express()
 
-// use morgan, helmet, bodyParser for loggin security and parsing the body respectively
+// set up mongo connection
+
+// use helmet, bodyParser for loggin security and parsing the body respectively
 app.use(helmet())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 app.enable('trust proxy')
+
+// creating a path to the build folder
+app.use(express.static(path.join(__dirname, 'build')))
 
 // setting the ports
 const port = process.env.PORT || '8080'
