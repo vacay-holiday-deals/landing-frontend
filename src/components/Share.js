d1e2433a1ffbd6c33916e056ae8b175def76e9d0
newtfrank <mymbugua@gmail.com>
2019-08-21 09:32:38 +0300

    added facebook pixel tracking && google analytics tracking

--- src/components/Share.js
+++ src/components/Share.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShareAlt } from '@fortawesome/free-solid-svg-icons'
 import Navbar from 'react-bootstrap/Navbar'
+import { TrackEvent } from '../components/tracking/facebookTracking'
+import { Event } from '../components/tracking/googleTracking'
 
 function Share({ title }) {
   const url = window.location.href
@@ -17,6 +19,10 @@ function Share({ title }) {
           href={`https://www.facebook.com/sharer/sharer.php?u=${newUrl}`}
           className='facebook'
           target='_blank'
+          onClick={() => {
+            Event('SHARING', 'share to facebook', 'SHARED_TO_FACEBOOK')
+            TrackEvent('SHARING', 'SHARED_TO_FACEBOOK')
+          }}
           rel='noopener noreferrer'>
           <span className='fontawesomeicon'>
             <FontAwesomeIcon icon={faShareAlt} className='icon' />
@@ -27,6 +33,10 @@ function Share({ title }) {
           href={`http://twitter.com/share?url=${newUrl}`}
           className='twitter'
           rel='noopener noreferrer'
+          onClick={() => {
+            Event('SHARING', 'share to Twitter', 'SHARED_TO_TWITTER')
+            TrackEvent('SHARING', 'SHARED_TO_TWITTER')
+          }}
           target='_blank'>
           <span className='fontawesomeicon'>
             <FontAwesomeIcon icon={faShareAlt} className='icon' />
@@ -37,6 +47,10 @@ function Share({ title }) {
           href={`whatsapp://send?text=${newUrl}`}
           className='whatsapp'
           rel='noopener noreferrer'
+          onClick={() => {
+            Event('SHARING', 'share to whatsapp', 'SHARED_TO_WHATSAPP')
+            TrackEvent('SHARING', 'SHARED_TO_WHATSAPP')
+          }}
           target='_blank'>
           <span className='fontawesomeicon'>
             <FontAwesomeIcon icon={faShareAlt} className='icon' />
